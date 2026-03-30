898352414bb7ba175323658165d49cb91c6ec9ca
Stefan-Emanuel Ancuta <sancuta@c2r5p14.42vienna.com>
2026-03-30 11:24:20 +0200

    final cherry on top, the program doesnt fail on less than 2 inputs, it just doesnt do anything

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sancuta <sancuta@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 12:04:26 by sancuta           #+#    #+#             */
-/*   Updated: 2026/03/30 11:11:58 by sancuta          ###   ########.fr       */
+/*   Updated: 2026/03/30 11:20:26 by sancuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,7 @@ int	main(int argc, char **argv)
 {
 	t_env	env;
 
-	if (argc < 2)
-		return (1);
-	if (argc == 2)
+	if (argc <= 2)
 		return (0);
 	env = init_env(argc);
 	parse_argv(&env, argc, argv);
@@ -62,8 +60,10 @@ int	main(int argc, char **argv)
 	if (argc < 6)
 		mini_sort(&env, argc - 1);
 	else
+	{
 		radix_sort(&env, argc - 1);
-	rotate_to_top(&env, find_min_pos(&env),
-		argc - 1);
+		rotate_to_top(&env, find_min_pos(&env),
+			argc - 1);
+	}
 	cleanup(env.node, "", EXIT_SUCCESS);
 }
